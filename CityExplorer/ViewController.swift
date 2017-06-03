5f8cac21bd15d72ba66fae38569b598eed0f2a3b
ManiuSuroiu <maniu.suroiu@gmail.com>
2017-06-03 11:10:59 +0100

    Initial Commit

---
+++ CityExplorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CityExplorer
+//
+//  Created by Maniu Suroiu on 03/06/2017.
+//  Copyright © 2017 Maniu Suroiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
