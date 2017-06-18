9b46c210db5e8025e9e70052eca6227517f671ba
ManiuSuroiu <maniu.suroiu@gmail.com>
2017-06-18 17:10:35 +0100

    Complete the functionality of the CLLocationManager (determine the location authorisation status, update the location of the user, implement the location manager delegate, check and handle location related errors, format the coordinates as required by the Foursquare API)

--- CityExplorer/LocationAccessDeniedViewController.swift
+++ CityExplorer/LocationAccessDeniedViewController.swift
@@ -14,16 +14,26 @@ class LocationAccessDeniedViewController: UIViewController {
     
     @IBOutlet weak var goToSettingsButton: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(dismissIfLocationAccessAuthorizedWhenInUse), name: .UIApplicationDidBecomeActive, object: nil)
-    }
-    
+    /* prompts the user to the Settings app to change the status of the Location Services */
     @IBAction func goToSettings(_ sender: Any) {
         UIApplication.shared.open(URL(string: UIApplicationOpenSettingsURLString)!)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* notifies the view controller that the application is now in the foreground (by calling the AppDelegate applicationDidBecomeActive() method) and specifies the method responsible with dismissing the viewcontroller */
+        NotificationCenter.default.addObserver(self, selector: #selector(dismissIfLocationAccessAuthorizedWhenInUse), name: .UIApplicationDidBecomeActive, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        /* removes the observer created above, once the viewWillDisappear is being called (the view controller is about to be dismissed and therefore the observer added must be removed */
+        NotificationCenter.default.removeObserver(self, name: .UIApplicationDidBecomeActive, object: nil)
+    }
+    
+    /* dismisses the view controller when the user has authorized location access */
     func dismissIfLocationAccessAuthorizedWhenInUse() {
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
             self.dismiss(animated: true, completion: nil)
--- CityExplorer/MapViewController.swift
+++ CityExplorer/MapViewController.swift
@@ -11,57 +11,59 @@ import MapKit
 
 class MapViewController: UIViewController {
     
+    // MARK: - IBOutlets
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var textField: UITextField!
     
+    // MARK: - Instance variables
     var locationManager = CLLocationManager()
     var startLocation: CLLocation?
+    var updatingLocation = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        determineLocationAuthStatus()
+    }
+    
+    /* determines the location authorization status and performs a certain action associated with that status (for instance if the status is "authorized when in use" it will call startLocationManager() method to start updating the user's location) */
+    func determineLocationAuthStatus() {
         let authStatus = CLLocationManager.authorizationStatus()
         
-        if authStatus == .notDetermined {
+        switch authStatus {
+        case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
-            return
-        }
-        
-        if authStatus == .denied || authStatus == .restricted {
+        case .denied, .restricted:
             perform(#selector(presentLocationAccessDeniedViewController), with: nil, afterDelay: 2)
-            return
+        case .authorizedWhenInUse, .authorizedAlways:
+            startLocationManager()
         }
-        
-        startLocationManager()
     }
     
-    func showLocationServicesDeniedAlert() {
-        let alert = UIAlertController(title: "Location Services Disabled",
-                                      message: "Please enable location services in your iPhone's Settings app!",
-                                      preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        
-        let goToSettingsAction = UIAlertAction(title: "Settings", style: .default) { _ in
-            UIApplication.shared.open(URL(string: UIApplicationOpenSettingsURLString)!)
-        }
-        
-        alert.addAction(okAction)
-        alert.addAction(goToSettingsAction)
-        
-        present(alert, animated: true, completion: nil)
-    }
-    
+    /* modal presentation of a new view controller - no segue - providing the user with info that location access hasn't been authorized and a button that prompts the user to the Settings app to change the authorization status to "allow location access when in use" */
     func presentLocationAccessDeniedViewController() {
         let viewController: LocationAccessDeniedViewController
         viewController = storyboard?.instantiateViewController(withIdentifier: "LocationAccessDeniedViewController") as! LocationAccessDeniedViewController
         present(viewController, animated: true, completion: nil)
     }
     
+    /* CLLocationManager into action - this is where it obtains GPS coordinates (by calling the startUpdatingLocation() method) */
     func startLocationManager() {
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
             locationManager.startUpdatingLocation()
+        }
+    }
+    
+    /* once the location manager begins updating user's location, put the latitude and longitude in the format required by the Foursquare API parameter (LatLongCoordinates - Constants.swift). Call this method from the delegate method locationManager(didUpdateLocations:) */
+    func formatGPSCoordinates() -> String {
+        if let location = startLocation {
+            let latitudeString = String(format: "%.8f", location.coordinate.latitude)
+            let longitudeString = String(format: "%.8f", location.coordinate.longitude)
+            return latitudeString + "," + longitudeString
+        } else {
+            return "Location coordinates could not be retrieved"
         }
     }
     
@@ -76,16 +78,46 @@ class MapViewController: UIViewController {
     */
 }
 
+    // MARK: - CLLocationManagerDelegate
+
 extension MapViewController: CLLocationManagerDelegate {
     
+    /* Tells the delegate that the location manager was unable to retrieve a location value. Uses a switch statemnet to look at the most common reasons that caused the error to occur. Each case creates an alert view controller to show the respective error to the user  */
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("didFailWithError \(error)")
+        
+        switch (error as NSError).code {
+            
+        case CLError.network.rawValue:
+            let alert = UIAlertController(title: "Network Error",
+                                          message: "The network is unavailable or a network error occurred",
+                                          preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            
+        case CLError.locationUnknown.rawValue:
+            let alert = UIAlertController(title: "Error Getting Location",
+                                          message: "The GPS is unable to obtain your location right now",
+                                          preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            
+        default:
+            let alert = UIAlertController(title: "An Error Has Occurred",
+                                          message: "Your location cannot be determined right now",
+                                          preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+        }
     }
     
+    /* Tells the delegate that new location data is available while storing it in a [ClLocation] array (second parameter) */
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let newLocation = locations.last!
         print("didUpdateLocations \(newLocation)")
         startLocation = newLocation
+        print(formatGPSCoordinates())
     }
 }
 
